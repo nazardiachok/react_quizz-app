bf5f89d0ec0cac2f8691ec299905d45b1b2f40b6
Nazar Diachok <diafcc55@gmail.com>
2022-09-28 19:33:42 +0200

    useState verwendet

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,44 +1,65 @@
 import "./footer.css";
+import { useState } from "react";
 
 export default function Footer() {
+  const [activePage, setActivePage] =
+    useState(
+      "Home"
+    ); /* wir denken uns die state "Home"(anfangsposition in useState) aus und jedes Mal beim click auf das konkretes button bekommt activePage in state ein anderes state. dann wird er verglichen und class angewendet */
+
   return (
-    <nav class="navigation">
-      <ul class="navigation__list">
-        <li class="navigation__list-item navigation__list-item--active">
-          <a
-            class="navigation__link"
-            href="./index.html"
-            aria-label="go to home page"
+    <nav className="navigation">
+      <ul className="navigation__list">
+        <li
+          className={`navigation__list-item ${
+            activePage === "Home" ? "navigation__list-item--active" : ""
+          }`}
+        >
+          <button
+            onClick={() => setActivePage("Home")}
+            className="navigation__link"
           >
             <img src="https://img.icons8.com/ios-filled/27/000000/home.png" />
-          </a>
+          </button>
         </li>
-        <li class="navigation__list-item">
-          <a
-            class="navigation__link"
-            href="./bookmarks.html"
-            aria-label="go to bookmarks page"
+
+        <li
+          className={`navigation__list-item ${
+            activePage === "Bookmark" ? "navigation__list-item--active" : ""
+          }`}
+        >
+          <button
+            onClick={() => setActivePage("Bookmark")}
+            className="navigation__link"
           >
             <img src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/30/000000/external-bookmark-multimedia-kiranshastry-lineal-kiranshastry.png" />
-          </a>
+          </button>
         </li>
-        <li class="navigation__list-item">
-          <a
-            class="navigation__link"
-            href="./form.html"
-            aria-label="go to form page"
+
+        <li
+          className={`navigation__list-item ${
+            activePage === "Form" ? "navigation__list-item--active" : ""
+          }`}
+        >
+          <button
+            onClick={() => setActivePage("Form")}
+            className="navigation__link"
           >
             <img src="https://img.icons8.com/external-simple-solid-edt.graphics/32/000000/external-Plus-add-and-remove-simple-solid-edt.graphics.png" />
-          </a>
+          </button>
         </li>
-        <li class="navigation__list-item">
-          <a
-            class="navigation__link"
-            href="./profile.html"
-            aria-label="go to profile page"
+
+        <li
+          className={`navigation__list-item ${
+            activePage === "Profile" ? "navigation__list-item--active" : ""
+          }`}
+        >
+          <button
+            onClick={() => setActivePage("Profile")}
+            className="navigation__link"
           >
             <img src="https://img.icons8.com/ios-glyphs/27/000000/parse-resumes.png" />
-          </a>
+          </button>
         </li>
       </ul>
     </nav>
--- src/components/main/card/card.js
+++ src/components/main/card/card.js
@@ -1,22 +1,32 @@
 import Bookmark from "../../images/icons8-lesezeichen-30.png";
 import BookmarkAktive from "../../images/icons8-lesezeichen-30 (1).png";
 import Tegform from "./tegsForm";
+import { useState } from "react";
 
 export default function Card({ question, answer }) {
   const tegs = [{ teg: "#when?" }, { teg: "#what?" }, { teg: "#why?" }];
+  const [showAnswer, setShowanswer] = useState(false); /*  */
 
   return (
     <li class="card-list__item">
       <article class="card" data-js="card">
         <h2 class="card__question">{question}</h2>
         <button
+          onClick={() => setShowanswer(!showAnswer)} /*  */
           class="card__button-answer"
           type="button"
           data-js="answer-button"
         >
-          Show answer
+          {showAnswer ? "Hide answer" : "Show answer"}
+          {/*  */}
         </button>
-        <p class="card__answer" data-js="answer">
+        <p
+          class={`card__answer ${
+            showAnswer ? "card__answer--active" : ""
+          }`} /*  */
+          data-js="answer"
+        >
+          {/*  */}
           {answer}
         </p>
         <ul class="card__tag-list">
